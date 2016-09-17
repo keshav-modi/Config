cae571a853dec8b1a733de56cc45686e8b90a437
Keshav Modi <keshav.modi@bankbazaar.com>
2016-09-17 09:07:10 +0530

    Cleaning Repo

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	@echo "love, not war"
-
-love:
-	@echo "not war"
--- xdg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#Naveen Kumar Molleti
-#08 December 2011
-
-if [ -z $XDG_CONFIG_HOME ]; then
-	export XDG_CONFIG_HOME="$HOME/.config"
-fi
